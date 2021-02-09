e7ede48da98d697a3b27a0cdad533124d33a23c0
Kira Hike <kirahikes@gmail.com>
2021-02-09 11:21:13 -0800

    put form input values in object pushed to data entries array incremented nextentryid | Kira Hike

--- js/data.js
+++ js/data.js
@@ -6,3 +6,22 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var $newEntryForm = document.querySelector('#entry-form');
+
+function clickSave(event) {
+  event.preventDefault();
+  var titleValue = $newEntryForm.elements.title.value;
+  var notesValue = $newEntryForm.elements.notes.value;
+  var imageValue = $newEntryForm.elements.url.value;
+  var entryObj = {
+    URL: imageValue,
+    title: titleValue,
+    notes: notesValue,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.push(entryObj);
+}
+
+$newEntryForm.addEventListener('submit', clickSave);
