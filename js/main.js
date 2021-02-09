f5f2de71d8f0a6eae76fdb6b062181bdba1e992e
Kira Hike <kirahikes@gmail.com>
2021-02-08 17:31:47 -0800

    added eventlistener to image input | Kira Hike

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+
+var $imageInput = document.querySelector('#image-url');
+var $image = document.querySelector('#image');
+
+$imageInput.addEventListener('blur', addImage);
+
+function addImage(event) {
+  $image.setAttribute('src', $imageInput.value);
+}
