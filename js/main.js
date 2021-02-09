496a992d55e8683d2b99e58ed23a0dcbfcf46c6d
Kira Hike <kirahikes@gmail.com>
2021-02-09 11:12:32 -0800

    moved form event listener to data.js | Kira Hike

--- js/main.js
+++ js/main.js
@@ -4,15 +4,8 @@
 var $imageInput = document.querySelector('#image-url');
 var $image = document.querySelector('#image');
 
-$imageInput.addEventListener('blur', addImage);
-
 function addImage(event) {
   $image.setAttribute('src', $imageInput.value);
 }
 
-var $buttonSave = document.querySelector('.button-save');
-
-$buttonSave.addEventListener('click', clickSave);
-
-function clickSave(event) {
-}
+$imageInput.addEventListener('blur', addImage);
