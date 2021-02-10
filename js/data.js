0b8dca6562bf59bcb7a197b0b1724cc7f68852ae
Kira Hike <kirahikes@gmail.com>
2021-02-09 17:18:48 -0800

    added JSON local storage functionality | Kira Hike

--- js/data.js
+++ js/data.js
@@ -6,3 +6,16 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', store);
+
+var previousEntriesJSON = localStorage.getItem('js-local-storage');
+
+if (previousEntriesJSON !== null) {
+  data = JSON.parse(previousEntriesJSON);
+}
+
+function store(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('js-local-storage', dataJSON);
+}
